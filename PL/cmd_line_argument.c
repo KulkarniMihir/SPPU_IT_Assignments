37b778709dbe11b449092fb8ae8b16854f817139
Saarth Deshpande <deshpandesaarth@gmail.com>
2019-08-31 11:42:42 +0530

    Update cmd_line_argument.c

--- PL/cmd_line_argument.c
+++ PL/cmd_line_argument.c
@@ -1,13 +1,3 @@
-/*
- ============================================================================
- Name        : cmd_line_argument.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
-
 #include <stdio.h>
 #include<string.h>
 #include <stdlib.h>
